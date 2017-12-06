7f985b3a04085a176d6f64cda9a33a2f14cc044a
Jessica Meyer <jessicameyer@Jessicas-MacBook-Pro.local>
2017-12-06 16:56:21 +0000

    my first commit

---
+++ twitter.js
@@ -0,0 +1,43 @@
+let tweetsElement = document.getElementById("tweets");
+let newTweetTextElement = document.getElementById("newTweetText");
+let submitTweetButton = document.getElementById("submitTweet");
+
+
+function addnewTweet(tweetHandle, tweetText) {
+  let newHandleElement = document.createElement("p");
+  newHandleElement.classList.add("handle");
+  newHandleElement.innerText = tweetHandle + ":";
+  //console.log("Our new tweet element", newTweetElement);
+  tweetsElement.appendChild(newHandleElement);
+
+  let newTweetElement = document.createElement("p");
+  newTweetElement.innerText = tweetText;
+  console.log("Our new tweet element", newTweetElement);
+  tweetsElement.appendChild(newTweetElement);
+
+
+
+
+}
+
+submitTweetButton.onclick = function() {
+    addnewTweet(newTweetTextElement.value);
+
+};
+
+//addnewTweet("This is another new tweet");
+//addnewTweet("We reached 1B notifications");
+//addnewTweet("It's 35 days til Xmas");
+
+// Enable pusher logging - don't include this in production
+   Pusher.logToConsole = true;
+
+   var pusher = new Pusher('6a162136f6f63f5b1568', {
+     cluster: 'eu',
+     encrypted: true
+   });
+
+   var channel = pusher.subscribe('my-channel');
+   channel.bind('my-event', function(data) {
+     addnewTweet(data.name,  data.message);
+   });
